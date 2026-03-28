2278256710cefb307836318a0d695862f5af1339
Michael Bestas <mkbestas@gmail.com>
2026-03-29 01:23:27 +0200

    muzel/rango: Download android15-6.6-2025-08_r31 GKI
    Change-Id: I76f7c4032a0f371a9c9f8504b5861059d212e665

--- google/rango/build_rango.sh
+++ google/rango/build_rango.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
 
-python3 download_prebuilts.py --force --build_target kernel_aarch64 --build_id 14239520
-python3 download_prebuilts.py --force --build_target kernel_aarch64_16k --build_id 14239520
+python3 download_prebuilts.py --force --build_target kernel_aarch64 --build_id 14739733
+python3 download_prebuilts.py --force --build_target kernel_aarch64_16k --build_id 14739733
 
 exec tools/bazel run \
   --config=stamp \

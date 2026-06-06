cce338eb37f947a7364c7e2f19090a0d9dbe4df6
Michael Bestas <mkbestas@gmail.com>
2026-06-06 23:09:28 +0300

    common: Download android15-6.6-2025-10_r22 GKI
    Matches 16 QPR3 Pixel kernel source.
    
    Change-Id: I5a0a22c37312c15e6602d4ffa35379975dbb8595

--- google/common/shell_utils.sh
+++ google/common/shell_utils.sh
@@ -29,8 +29,8 @@ function gettop
 }
 
 function download_gki_prebuilts() {
-  python3 download_prebuilts.py --force --build_target kernel_aarch64 --build_id 15260253
-  python3 download_prebuilts.py --force --build_target kernel_aarch64_16k --build_id 15260253
+  python3 download_prebuilts.py --force --build_target kernel_aarch64 --build_id 14739656
+  python3 download_prebuilts.py --force --build_target kernel_aarch64_16k --build_id 14739656
 }
 
 function setup_cog_env_if_needed() {

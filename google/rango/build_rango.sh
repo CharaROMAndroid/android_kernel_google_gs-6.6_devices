4716e2b21270b2a75c08c6219a467a984d98c6e4
Michael Bestas <mkbestas@gmail.com>
2026-06-06 21:17:21 +0300

    Import kernel-muzel-14782430 changes
    Change-Id: I68fbe0dd25c010e7a844faded49b5fb53a6f1d09

--- google/rango/build_rango.sh
+++ google/rango/build_rango.sh
@@ -4,6 +4,9 @@
 python3 download_prebuilts.py --force --build_target kernel_aarch64 --build_id 15260253
 python3 download_prebuilts.py --force --build_target kernel_aarch64_16k --build_id 15260253
 
+source private/devices/google/common/shell_utils.sh
+setup_cog_env_if_needed
+
 exec tools/bazel run \
   --config=stamp \
   --config=rango \
